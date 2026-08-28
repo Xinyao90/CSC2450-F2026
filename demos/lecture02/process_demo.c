76fc684e8e935dbb7fd8f0dfc49dd6af246e15c4
Xinyao90 <lixinyao90@gmail.com>
2026-08-28 11:17:56 -0500

    add process_demo.c

---
+++ demos/lecture02/process_demo.c
@@ -0,0 +1,21 @@
+#include <stdio.h>      // printf()
+#include <unistd.h>     // getpid(), getppid(), sleep()
+
+int main()
+{
+    // PID = ID of this running process
+    printf("My PID  = %d\n", getpid());
+
+    // PPID = ID of the process that started this process
+    // Usually this will be the shell
+    printf("My PPID = %d\n", getppid());
+
+    printf("Process is running for 300 seconds...\n");
+
+    // Keep the process alive so we have time to inspect it
+    sleep(300);
+
+    printf("Process finished.\n");
+
+    return 0;
+}
